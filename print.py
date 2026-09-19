0663ffee17cabb6dccaed6b0b809a782cf3d8911
PavelName <102708206+PavelName@users.noreply.github.com>
2026-09-19 18:57:08 +0300

    continue for loop

--- print.py
+++ print.py
@@ -845,3 +845,60 @@ num = 7
 #     for j in range(2):
 #         print('/', end='+')
 
+# for i in range(2):
+#     for j in range(2):
+#         print('*', end='#')
+
+#     for k in range(3):
+#         print('%', end='&')
+
+#     print()
+
+# for i in range(1, 4):
+#     for j in range(3, 5):
+#         print(i + j, end='')
+
+
+# counter = 0
+# for i in range(99, 102):
+#     temp = i
+#     while temp > 0:
+#         counter += 1
+#         temp //= 10
+# print(counter)
+
+# n = int(input())
+# for _ in range(n):
+#     print(n, n, n)
+
+# n = int(input())
+# for i in range(1, n + 1):
+#     print(i, i, i, i, i)
+
+# n = int(input())
+
+# for i in range(1, n + 1):
+#     for j in range(1, 10):
+#         print(i, '+', j, '=', i + j)
+
+#     print()
+
+# n = int(input())
+
+# for i in range(1, n + 1):
+#     for j in range(i):
+#         print(i, end='')
+#     print()
+
+# n = int(input())
+
+# for i in range(1, n // 2 + 2):
+#     for _ in range(i):
+#         print('*', end='')
+#     print()
+
+# for i in range(n // 2, 0, -1):
+#     for _ in range(i):
+#         print('*', end='')
+#     print()
+
